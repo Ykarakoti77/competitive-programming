d2de7988a88998d1f5097e44e16c4b22c8a6649a
Ykarakoti77 <97166176+Ykarakoti77@users.noreply.github.com>
2022-09-18 19:03:23 +0530

    Create sparse_table.cpp

---
+++ Sparse Table/sparse_table.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_N = 100'005;
+const int LOG = 17;
+int a[MAX_N];
+int m[MAX_N][LOG]; // m[i][j] is minimum in the range starting from i and of length 2 ^ j
+int bin_log[MAX_N];
+
+int query(int L, int R) { // O(1)
+	int length = R - L + 1;
+	int k = bin_log[length];
+	return min(m[L][k], m[R-(1<<k)+1][k]);
+}
+
+int main() {
+	// 1) read input
+	int n;
+	cin >> n;
+	
+  bin_log[1] = 0;
+	for(int i = 2; i <= n; i++) {
+		bin_log[i] = bin_log[i/2]+1;
+	}
+	
+  for(int i = 0; i < n; i++) {
+		cin >> a[i];
+		m[i][0] = a[i];
+	}
+  
+	// 2) preprocessing, O(N*log(N))
+	for(int k = 1; k < LOG; k++) {
+		for(int i = 0; i + (1 << k) - 1 < n; i++) {
+			m[i][k] = min(m[i][k-1], m[i+(1<<(k-1))][k-1]);    
+		}
+	}
+	// 3) answer queries
+	int q;
+	cin >> q;
+	for(int i = 0; i < q; i++) {
+		int L, R;
+		cin >> L >> R;
+		cout << query(L, R) << "\n";
+	}
+}
