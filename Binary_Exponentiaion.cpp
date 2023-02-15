d556c2bdc0a91f0e265084402ede84939a735427
Ykarakoti77 <97166176+Ykarakoti77@users.noreply.github.com>
2023-02-15 15:53:07 +0530

    precomputed inverse

--- Binary_Exponentiaion.cpp
+++ Binary_Exponentiaion.cpp
@@ -3,8 +3,9 @@ using namespace std;
 #define ll long long
 
 ll M = 1e9 + 7;
-const ll N = 1002;
+const ll N = 3000007;
 ll fact[N];
+ll inv[N];
 
 ll binpow(ll a, ll b) {  // for calculating inverse use binpow(a, M-2);
     ll res = 1;
@@ -23,28 +24,23 @@ void facto(ll n) {
         fact[i] = (fact[i - 1] * i) % M;
     }
 }
+// we can also precompute inverse rather then computing it when inside nCr everytime
+void inverse(ll n) {
+	inv[0] = 1;
+	inv[n] = binpow(fact[n], M - 2);
+	for (ll i = n; i > 0; i--){
+		inv[i - 1] = (inv[i] * i) % M;
+	}
+}
 
 ll nCr(ll n, ll r) {
-    ll ans = (fact[n] * binpow(fact[r], M - 2) % M * binpow((fact[n - r]), M - 2) % M) % M;
-    return ans;
+	ll ans = (fact[n] * inv[r] % M * inv[n - r] % M) % M;
+	return ans;
 }
 
 /**/
 void solve() {
-    ll n, k;
-    cin >> n >> k;
-    vector<ll> v(n);
-    for (auto &i : v) cin >> i;
-
-    sort(v.begin(), v.end());
-    ll num = v[n - k];
-    ll cnt = 0, z = 0;
-    for (ll i = 0; i < n; i++) {
-        if (v[i] == num) cnt++;
-        if (v[i] == num && i >= n - k) z++;
-    }
-    ll ways = nCr(cnt, z);
-    cout << ways << '\n';
+   
 }
 
 int main() {
@@ -53,6 +49,7 @@ int main() {
     ll t = 1;
     cin >> t;
     facto(N);
+    inverse(N);
     while (t--) {
         solve();
     }
